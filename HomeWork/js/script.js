9a5a46607f3bad1b1cb6a290eaffdd086b23c41d
Sofiia Barylska <sofiia.barylska@smile-ukraine.com>
2024-06-24 11:49:12 +0300

    Answers for homework 6

---
+++ HomeWork/js/script.js
@@ -0,0 +1,43 @@
+/* Створити телефонну книгу
+створити початковий клас Abonent, де зберігатимуться ім * я і номер
+створити set який прийматиме телефон і номер
+створити get який виводитиме данні про абонента
+створити три різних юзери
+вивести данні */
+
+/* class Abonent{
+    name = "";
+    phoneNumber = "";
+
+    set abonentName(names) {
+        this.name = names;
+}
+    set abonentNumber(phone) {
+        this.phoneNumber = phone;
+ }
+  
+    get abonentName() {
+        return this.name;
+    }
+     get abonentNumber() {
+        return this.phoneNumber;
+    }
+    
+}
+
+const Rubi = new Abonent()
+Rubi.abonentName = "Rubi Barylskiy";
+ Rubi.abonentNumber = "084774848";
+;
+const Marko = new Abonent()
+    Marko.abonentName = "Marko Omelyasha";
+Marko.abonentNumber = "34234232";
+;
+const Kay = new Abonent()
+Kay.abonentName = "Kay Family";
+Kay.abonentNumber = "23434456";
+;
+
+console.log("Name of abonent is ", Rubi.abonentName, " phone number of abonent is", Rubi.abonentNumber);
+console.log("Name of abonent is", Marko.abonentName, ", phone number of abonent is", Marko.abonentNumber);
+console.log("Name of abonent is", Kay.abonentName, ", phone number of abonent is", Kay.abonentNumber); */
